c6c0c400f3d8e2f9740c7a96e7aac1108145ddfb
Sotirios Pupakis <sotirios.pupakis@gmail.com>
2019-12-10 07:49:54 +0100

    Again... wtf

---
+++ cmake-build-debug-mingw/CMakeFiles/proj3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proj3.dir/proj3.c.obj"
+  "libproj3.dll.a"
+  "proj3.exe"
+  "proj3.exe.manifest"
+  "proj3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/proj3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
